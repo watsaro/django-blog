acf9ba466115e258325d9e65e7953503699d1dbf
Aron Watson <watsaro@gmail.com>
2020-05-17 19:37:23 -0700

    finished

--- blogging/views.py
+++ blogging/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect, Http404
-
+from django.template import loader
+from blogging.models import Post
+from django.shortcuts import render
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
     if args:
@@ -9,3 +11,18 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
--- mysite/settings.py
+++ mysite/settings.py
@@ -126,3 +126,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
